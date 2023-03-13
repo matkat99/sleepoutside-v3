cc2b9e82a58efff21caa14f4821edbe24261e31d
Shane T <st83440@gmail.com>
2023-03-13 14:28:20 -0600

    Initialize v3-team3

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+      },
+    },
+  },
+});
