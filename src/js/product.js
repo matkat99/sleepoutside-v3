b1db9fe24f0138a1927632d417fd28a9d260ca77
Shane T <st83440@gmail.com>
2023-03-13 14:28:15 -0600

    Initialize vite-team3

---
+++ src/js/product.js
@@ -0,0 +1,9 @@
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
